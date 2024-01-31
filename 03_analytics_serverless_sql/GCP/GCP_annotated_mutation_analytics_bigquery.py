496565f8204021118e802dddf28b5a29ab1c5c8b
doit-mattporter <matt.porter@doit.com>
2024-01-30 16:46:20 -0800

    Added BQ data load script BQ query script now runs queries and calculates costs as expected

--- 03_analytics_serverless_sql/GCP/GCP_annotated_mutation_analytics_bigquery.py
+++ 03_analytics_serverless_sql/GCP/GCP_annotated_mutation_analytics_bigquery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from google.cloud import bigquery
 import configparser
+import pandas as pd
 import time
 
 # Load configuration values
@@ -9,7 +10,7 @@ config.read("../../config.ini")
 dataset_bucket = config["GCPConfig"]["OUTPUT_BUCKET"]
 bigquery_project = config["GCPConfig"]["BIGQUERY_PROJECT"]
 bigquery_dataset = config["GCPConfig"]["BIGQUERY_DATASET"]
-cost_per_tb = config["GCPConfig"]["BIGQUERY_COST_PER_TB"]  # Cost per TB processed in USD for BigQuery on-demand
+cost_per_tb = float(config["GCPConfig"]["BIGQUERY_COST_PER_TB"])  # Cost per TB processed in USD for BigQuery on-demand
 
 client = bigquery.Client(project=bigquery_project)
 
@@ -24,13 +25,16 @@ total_bytes_processed = 0
 # Execute each SQL command and report on its cost
 for i, sql in enumerate(cmd for cmd in sql_commands if cmd.strip()):
     print(f"Executing Query {i+1}\n{sql}\n")
-    query_job = client.query(sql.strip())
+    job_config = bigquery.QueryJobConfig(use_query_cache=False)
+    query_job = client.query(sql.strip(), job_config=job_config)
     try:
         results = query_job.result()
+        df = results.to_dataframe()
         bytes_processed = query_job.total_bytes_processed
         total_bytes_processed += bytes_processed
         query_cost = (bytes_processed / 1e12) * cost_per_tb
-        print(f"Query {i+1} processed {bytes_processed / 1e9:.2f} GB of data. Cost: ${query_cost:.2f}")
+        print(f"Query {i+1} processed {bytes_processed / 1e9:.2f} GB of data. Cost: ${query_cost:.2f}\n")
+        print(df.head(10))
     except Exception as e:
         print(f"Error in query {i+1}: {e}")
 
